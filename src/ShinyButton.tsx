1e55ef06bc313c44fb4460160cf24cf28ca2890a
assilbekov <askhat.assilbekov@gmail.com>
2024-04-22 05:42:10 -0300

    init animation

--- src/ShinyButton.tsx
+++ src/ShinyButton.tsx
@@ -1,9 +1,19 @@
+import { motion } from "framer-motion";
+
 export const ShinyButton = () => {
   return (
-    <button className="px-6 py-2 rounded-md relative radial-gradient">
+    <motion.button
+      className="px-6 py-2 rounded-md relative radial-gradient"
+      initial={{ "--x": "100%" }}
+      animate={{ "--x": "-100%" }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+      }}
+    >
       <span className="text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask">
         Start now
       </span>
-    </button>
+    </motion.button>
   );
 };
