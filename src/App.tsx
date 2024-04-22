15b2adb88de1fa7e9650b23beedb96adc40da4b7
assilbekov <askhat.assilbekov@gmail.com>
2024-04-22 05:34:06 -0300

    add button bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ShinyButton } from "./ShinyButton";
 
 function App() {
   return (
-    <div className="bg-slate-950 w-full h-screen flex items-center justify-center">
+    <div className="bg-neutral-950 w-full h-screen flex items-center justify-center">
       <ShinyButton />
     </div>
   );
