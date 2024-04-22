a2282daf35396a534f06bad805a9284663d0073f
assilbekov <askhat.assilbekov@gmail.com>
2024-04-22 05:45:23 -0300

    add scale animation

--- src/ShinyButton.tsx
+++ src/ShinyButton.tsx
@@ -15,6 +15,12 @@ export const ShinyButton = () => {
         stiffness: 20,
         damping: 15,
         mass: 2,
+        scale: {
+          type: "spring",
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1
+        }
       }}
     >
       <span className="text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask">
