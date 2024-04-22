41f77294bcb4b8ee035fe24c4da67ed56793071b
assilbekov <askhat.assilbekov@gmail.com>
2024-04-22 05:27:10 -0300

    start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ShinyButton } from "./ShinyButton";
 
 function App() {
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ShinyButton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ShinyButton.tsx
@@ -0,0 +1,7 @@
+
+
+export const ShinyButton = () => {
+  return (
+    <button>Start now</button>
+  )
+}
